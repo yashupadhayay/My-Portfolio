97c1424693b6e5ca417696433562f5010060322e
yashupadhayay <162331806+yashupadhayay@users.noreply.github.com>
2024-03-12 23:11:20 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,6 @@
+let dayNight = document.querySelector(".dayNight");
+let Portfolio = document.querySelector(".Portfolio");
+
+dayNight.addEventListener("click",()=>{
+    Portfolio.classList.toggle("night");
+})
